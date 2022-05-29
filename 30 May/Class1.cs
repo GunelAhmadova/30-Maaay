3bf12acff3ddecf8c3527d8e0b6fc76633b6b889
txt5 <105361005+GunelAhmadova@users.noreply.github.com>
2022-05-29 19:55:26 +0400

    Add files via upload

---
+++ 30 May/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30_May
+{
+    class Notebook
+    {
+        public string Name;
+        public string Brandname;
+        public double Price;
+       
+
+      
+    } 
+    class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelFor1Km;
+        public double Millage;
+
+        public void Drive()
+        {
+            Console.WriteLine("nece km surmek isteyirsiz?");
+            double km = int.Parse(Console.ReadLine()); 
+            if (CurrentFuel> km * FuelFor1Km)
+            {
+                Console.WriteLine("kifayyet qeder benzinimiz var");
+                Millage += km;
+                CurrentFuel -= km * FuelFor1Km;
+                Console.WriteLine( $"gedisden sonra {CurrentFuel} litr qalacaq");
+
+            } 
+           
+
+            else  {
+              
+
+                Console.WriteLine("kifayyet qeder benzin yoxdur");
+            }
+        } 
+
+    }
+
+    
+}
---
+++ 30 May/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _30_May
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook[] notebooks = new Notebook[3];
+            notebooks[0] = new Notebook()
+            {
+                Name = "Acer",
+                Brandname = "aspire",
+                Price = 2000
+            };
+            notebooks[1] = new Notebook()
+            {
+                Name = "Hp",
+                Brandname = "Paviiion",
+                Price = 1500
+            };
+            notebooks[2] = new Notebook()
+            {
+                Name = "Lenovo",
+                Brandname = "Yoga",
+                Price = 2500
+            };
+
+            Console.WriteLine("Minimum qiymeti yazin");
+            int minPrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Maksimum qiymeti yazin");
+            int maxPrice = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < notebooks.Length; i++)
+            {
+                if (notebooks[i].Price > minPrice && notebooks[i].Price < maxPrice)
+                {
+                    Console.WriteLine($"{notebooks[i].Name} {notebooks[i].Brandname} {notebooks[i].Price}");
+
+                }
+
+
+            }
+
+            //Aşağıdaki parametrlərə sahib Car class yaradın
+            //         -Brand
+            //         - Model
+            //         - CurrentFuel - masinin hazirki benzin miqdari
+            //         - FuelFor1Km - masinin 1 km ucun xerclediyi benzin
+            // -Millage - Masinin bu vaxtadək neçə km sürüldüyü
+            //- Drive() - sürülmə metodu.Bu metod int parametr qeəbul edir(neçə km sürmək istədiyimiz)
+            //  Car obyekti yaradın drive metodunu işə saldıqda arqument olaraq neçə km sürmək istədyimizi
+            //  daxil edirik və əgər benzin kifayət edirsə maşının millage dəyəri və benzini azalır,
+            //  benzin kifayət etmirsə console - da xəta mesajı yazılır
+
+
+            Car car1 = new Car
+            {
+                Brand = "Ford",
+                Model = "Mustang",
+                CurrentFuel = 50,
+                FuelFor1Km = 1,
+                Millage = 30000
+            };
+
+
+        car1.Drive();
+            
+        }
+        
+        
+            
+       
+    }
+}
